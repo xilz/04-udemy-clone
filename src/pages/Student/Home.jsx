3831a0d5f5dcc9069f9e0b4116480adc840b062b
XilingZHU <zxl041024@163.com>
2025-08-05 20:07:39 +0800

    Student-Home-赞助商

--- src/pages/Student/Home.jsx
+++ src/pages/Student/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../../components/Student/Hero'
+import Companies from '../../components/Student/Companies'
 
 const Home = () => {
   return (
     <div className='flex flex-col items-center space-y-7 text-center'>
       <Hero />
+      <Companies />
     </div>
   )
 }
