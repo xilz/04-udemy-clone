44d39cdc3a6c26beb276c66a19b3152229905cd6
XilingZHU <zxl041024@163.com>
2025-08-06 14:15:31 +0800

    Student-Home-rating + nav交互

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,17 +1,30 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext()
 
 export const AppContextProvider = (props) => {
 
     const currency = import.meta.env.VITE_CURRENCY
+    const navigate = useNavigate()
 
     const [allCourses, setAllCourses] = useState([])
+    const [isEducator, setIsEducator] = useState(true)
 
     // Fetch all courses from backend
     const fetchAllCourses = async () => {
         setAllCourses(dummyCourses)
+    }
+
+    // Function to calculate average rating of course
+    const calculateRating = (course) => {
+        if (course.courseRatings.length === 0) return 0;
+        let totalRating = 0;
+        course.courseRatings.forEach(rating => {
+            totalRating += rating.rating;
+        });
+        return (totalRating / course.courseRatings.length).toFixed(1);
     }
     
 useEffect(() => {
@@ -19,7 +32,7 @@ useEffect(() => {
 }, [])
 
     const value = {
-        currency, allCourses
+        currency, allCourses, navigate, calculateRating, isEducator, setIsEducator,
     }
     return (
         <AppContext.Provider value={value}>
