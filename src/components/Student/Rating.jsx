5b4785bc239ab836ec4014bbe682dd554d1af586
XilingZHU <zxl041024@163.com>
2025-08-07 23:20:11 +0800

    Student-player&rating

--- src/components/Student/Rating.jsx
+++ src/components/Student/Rating.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 
-const Rating = () => {
+const Rating = ({ initialRating, onRate }) => {
+  const [rating, setRating] = useState(initialRating || 0);
+
+  const handleRatingClick = (value) => {
+    setRating(value);
+    if (onRate) onRate(value);
+  };
+
+  useEffect(() => {
+    setRating(initialRating || 0);
+  }, [initialRating]);
+
   return (
-    <div>Rating</div>
-  )
-}
+    <div>
+      {Array.from({ length: 5 }).map((_, index) => {
+        const starValue = index + 1;
+        return (
+          <span 
+            key={index} 
+            className={`text-xl px-1 sm:text-2xl cursor-pointer transition-colors ${
+              starValue <= rating ? 'text-yellow-500' : 'text-gray-400'
+            }`}
+            onClick={() => handleRatingClick(starValue)}
+          >
+            &#9733;
+          </span>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Rating
+export default Rating;
