80f1355820c00b91409c2799b27778d0505bbf72
XilingZHU <zxl041024@163.com>
2025-08-07 11:34:43 +0800

    一级页面-课程列表

--- src/components/Student/CallToAction.jsx
+++ src/components/Student/CallToAction.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
 
 const CallToAction = () => {
   return (
@@ -7,8 +8,8 @@ const CallToAction = () => {
       <h1 className='text-xl md:text-4xl text-gray-800 font-semibold'>Learn anything, anytime, anywhere</h1>
       <p className='text-gray-500 sm:text-sm'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis dolor corrupti sequi temporibus praesentium rem facilis.</p>
       <div className='flex items-center font-medium gap-6 mt-4'>
-        <button className='px-10 py-3 rounded-md text-white bg-blue-600'>Get started</button>
-        <button className='flex items-center gap-2'>Learn more <img src={assets.arrow_icon} alt="arrow_icon" /></button>
+        <Link to={'/course-list'} onClick={() => scrollTo(0, 0)} className='px-10 py-3 rounded-md text-white bg-blue-600'>Get started</Link>
+        <button className='flex items-center gap-2'>Learn more<img src={assets.arrow_icon} alt="arrow_icon" /></button>
       </div>
     </div>
   )
