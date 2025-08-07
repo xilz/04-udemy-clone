d0fbd86a3cb443b530b3810a3a37898c91c68bf4
XilingZHU <zxl041024@163.com>
2025-08-07 22:02:53 +0800

    Student-课程页详情-iframe试看

--- src/components/Student/Loading.jsx
+++ src/components/Student/Loading.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div><h1>Loading</h1></div>
+    <div>
+      <div className='w-16 sm:w-20 aspect-square border-4 border-gray-300 border-t-4 border-t-blue-400 rounded-full animate-spin'></div>
+    </div>
   )
 }
 
--- src/pages/Student/CourseDetails.jsx
+++ src/pages/Student/CourseDetails.jsx
@@ -5,6 +5,7 @@ import Loading from '../../components/Student/Loading'
 import { assets } from '../../assets/assets'
 import humanizeDuration from 'humanize-duration'
 import Footer from '../../components/Student/Footer'
+import YouTube from 'react-youtube'
 
 const CourseDetails = () => {
 
@@ -12,8 +13,10 @@ const CourseDetails = () => {
 
   const [courseData, setCourseData] = useState(null)
   const [openSection, setOpenSection] = useState({})
+  const [isAlreadyEnrolled, setIsAlreadyEnrolled] = useState(false)
+  const [playerData, setPlayerData] = useState(null)
 
-  const {allCourses, calculateRating, calculateChapterTime, calculateCourseDuration, calculateNoOfLectures, currency, isAlreadyEnrolled} = useContext(AppContext)
+  const {allCourses, calculateRating, calculateChapterTime, calculateCourseDuration, calculateNoOfLectures, currency} = useContext(AppContext)
   const fetchCourseData = async () => {
     const findCourse = allCourses.find(course => course._id === id)
     setCourseData(findCourse);
@@ -71,7 +74,11 @@ const CourseDetails = () => {
                         <div className='flex items-center justify-between w-full text-gray-800 text-xs md:text-default'>
                           <p>{lecture.lectureTitle}</p>
                           <div className='flex gap-2'>
-                            {lecture.isPreviewFree && <p className='text-blue-500 cursor-pointer'>Preview</p>}
+                            {lecture.isPreviewFree && <p 
+                            onClick={() => setPlayerData({
+                              videoId: lecture.lectureUrl.split('/').pop()
+                            })}
+                            className='text-blue-500 cursor-pointer'>Preview</p>}
                             <p>{humanizeDuration(lecture.lectureDuration * 60 * 1000, {units: ['h', 'm']})}</p>
                           </div>
                         </div>
@@ -93,10 +100,14 @@ const CourseDetails = () => {
       
       {/* right column */}
       <div className='max-w-course-card z-10 shadow-custom-card rounded-t md:rounded-none overflow-hidden bg-white min-w-[300px] sm:min-w-[420px]'>
-        <img src={courseData.courseThumbnail} alt="" />
+        {
+          playerData ? 
+            <YouTube videoId = {playerData.videoId} opts={{playerVars: {autoplay: 1}}} iframeClassName='w-full aspect-video' />
+          : <img src={courseData.courseThumbnail} alt="" />
+        }
         <div className='p-5'>
           <div className='flex items-center gap-2'>
-            <img className='w-3.5' src={assets.time_left_clock_icon} alt="" />
+            <img className='w-3.5' src={assets.time_left_clock_icon} />
             <p className='text-red-500'><span className='font-medium'>5 days</span> left at this price!</p>
           </div>
 
